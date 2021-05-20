8bd2479c8458619ba89790bf6c6b9a9ac520838c
Divit Jawa <divit.dj@hotmail.com>
2021-05-20 16:36:14 -0700

    last issues left

--- app/src/main/java/com/divitj/dotify1/MainActivity.kt
+++ app/src/main/java/com/divitj/dotify1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.divitj.dotify1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import kotlin.random.Random
 
@@ -13,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvNumberPlays: TextView
     private lateinit var etChangeUser: EditText
     private lateinit var tvCurrUser: TextView
+    private lateinit var btnChangeUser: Button
+    private lateinit var songCoverImg: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +27,10 @@ class MainActivity : AppCompatActivity() {
         btnPlay = findViewById(R.id.ibPlayBtn)
         etChangeUser = findViewById(R.id.etUsername)
         tvCurrUser = findViewById(R.id.tvCurrUser)
+        btnChangeUser = findViewById(R.id.btnChangeUser)
+        songCoverImg = findViewById(R.id.ivCoverArt)
+
+        etChangeUser.visibility = View.GONE
 
         btnPrevSong.setOnClickListener{
             Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
@@ -38,6 +46,27 @@ class MainActivity : AppCompatActivity() {
         btnPlay.setOnClickListener{
             plays += 1
             tvNumberPlays.text = "${plays.toString()} plays"
+        }
+
+        btnChangeUser.setOnClickListener {
+            if (btnChangeUser.text == this@MainActivity.getString(R.string.btnChangeUser)) {
+                tvCurrUser.visibility = View.GONE
+                etChangeUser.visibility = View.VISIBLE
+                btnChangeUser.text = getString(R.string.apply)
+            } else if(btnChangeUser.text == this@MainActivity.getString(R.string.apply)) {
+                tvCurrUser.text = etChangeUser.text
+                if (etChangeUser.text.isNullOrBlank()) {
+                    Toast.makeText(this, "user name field left empty", Toast.LENGTH_SHORT).show()
+                }
+                etChangeUser.visibility = View.GONE
+                tvCurrUser.visibility = View.VISIBLE
+                btnChangeUser.text = getString(R.string.btnChangeUser)
+            }
+        }
+
+        songCoverImg.setOnLongClickListener{
+            tvNumberPlays.setTextColor(getColor(R.color.spotify_green))
+            return@setOnLongClickListener true
         }
     }
 }
